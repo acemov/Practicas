a1eef8423afddbf114f89dbfdc29b8d3e58d97b4
FrancoEMTG <aesevv@gmail.com>
2023-08-20 20:59:36 -0300

    agregando comentarios

--- practicando8/app.js
+++ practicando8/app.js
@@ -3,15 +3,21 @@ const app = express();
 
 const cursosImportados = require('./cursos.js')
 
+// PARA HACER CORRRER EL SERVIDOR
+app.listen(3000, () => {
+    console.log('todo bien')
+})
+
+// PARA HACER LAS RUTAS
 app.get('/', function (req, res) {
     res.send("mi primer servidor")
 })
 
-app.get('/api/cursos', function (req, res) {
+app.get('/cursos', function (req, res) {
     res.send(cursosImportados)
 })
 
 
-app.listen(3000, () => {
-    console.log('todo bien')
+app.get("/probando",function(req,res){
+    res.send("probando")
 })
